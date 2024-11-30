0b19a8648fe3ba06e0d5aecdc1bb51cb2387f613
diegocamara89 <marcelodiego@gmail.com>
2024-11-30 00:06:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new Client({
     }),
     puppeteer: {
         headless: true,
-        product: 'chrome',
+        executablePath: '/usr/bin/google-chrome-stable',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
